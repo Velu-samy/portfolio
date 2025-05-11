de104580e198d1ea98067a3944b6ea5b62304263
velu-samy <velusamyplay0007@gmail.com>
2025-05-11 19:54:36 +0530

    blog websit added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,6 +4,7 @@ import atm from '../asstes/python.jpg';
 import ticket from '../asstes/ticket.jpg';
 import cs from '../asstes/cw.png';
 import car from '../asstes/car.png';
+import blog from '../asstes/blog.png';
 const Projects = () => {
   return (
     <div className='mb-10'>
@@ -121,6 +122,41 @@ const Projects = () => {
         <div className="button-container flex lg:justify-start mt-4 justify-between  lg:gap-80">
          <a href="https://github.com/Velu-samy/Luxecar    "className='px-4 py-1 border border-pink-500'>GitHub</a>
          <a href= "https://luxecar-tau.vercel.app/  " className='px-4 py-1 border border-pink-500'>Live</a>
+
+        </div>
+        </motion.div>
+      </div>
+      <div className="row flex  flex-col  lg:flex-row p-5  lg:px-40 font-tittle text-neutral-100 mt-5 items-center">
+        <motion.div whileInView={{opacity:1,x:0}} 
+              initial={{opacity:0,x:-100}}
+          transition={{duration:0.8,delay:0.6}}     className="col-1 w-full  lg:w-1/2 flex  lg:justify-center">
+            <img src={blog} className='rounded-md w-80 border border-transparent hover:border-purple-500' alt="" />
+        </motion.div>
+        <motion.div whileInView={{opacity:1,x:0}} 
+              initial={{opacity:0,x:100}}
+          transition={{duration:0.8,delay:0.6}}      className="col-2 w-full  lg:w-1/2  mt-3 lg:mt-0">
+        <h2 className='font-semibold mb-1'>Tech Hub Blog</h2>
+      <ul className='list-disc font-sans font-thin text-sm mt-1'>
+  <li>Built a full-stack blog platform with CRUD & JWT authentication.</li>
+  <li>Integrated <strong>Cloudinary</strong> for seamless image uploads.</li>
+  <li>Designed a responsive UI and improved user experience with <strong>Framer Motion</strong>.</li>
+  <li>Optimized authorization flows for secure sessions.</li>
+  <li>Refactored backend queries, boosting performance by <strong>40%</strong>.</li>
+</ul>
+
+<div className="tech-con flex  justify-start mt-3 flex-wrap gap-2 opacity-70">
+          <p className='mr-3 rounded-sm border-purple-600 text-purple-400 bg-black border p-0.5 px-1.5'>React</p>
+                    <p className='mr-3 rounded-sm border-purple-600 text-purple-400 bg-black border p-0.5 px-1.5'>Node</p>
+          <p className='mr-3 rounded-sm border-purple-600 text-purple-400 bg-black border p-0.5 px-1.5'>Express node</p>
+          <p className='mr-3 rounded-sm border-purple-600 text-purple-400 bg-black border p-0.5 px-1.5'>MongoDb</p>
+                    <p className='mr-3 rounded-sm border-purple-600 text-purple-400 bg-black border p-0.5 px-1.5'>JWT</p>
+                              <p className='mr-3 rounded-sm border-purple-600 text-purple-400 bg-black border p-0.5 px-1.5'>Cloudnary</p>
+          <p className='mr-3 rounded-sm border-purple-600 text-purple-400 bg-black border p-0.5 px-1.5'>Framer Motion</p>
+
+        </div>
+        <div className="button-container flex lg:justify-start mt-4 justify-between  lg:gap-80">
+         <a href="https://github.com/Velu-samy/MERN-Blog"className='px-4 py-1 border border-pink-500'>GitHub</a>
+         <a href= "https://mern-blog-f.onrender.com " className='px-4 py-1 border border-pink-500'>Live</a>
 
         </div>
         </motion.div>
